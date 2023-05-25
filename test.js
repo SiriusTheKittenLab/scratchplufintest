47febb3e96cc1c491cbecc5e7ff87853f6106c68
ChucklesTheGuineaPig <112353565+SiriusTheKittenLab@users.noreply.github.com>
2023-05-24 19:27:16 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,31 @@
-(function (ext) {
-    // Block descriptor
-    var descriptor = {
-        blocks: [
-            ['w', 'do %n times', 'doTimes'],
-            ['w', 'then do %n times', 'thenDoTimes'],
-        ],
-    };
+class ScratchFetch {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Fetch",
+            "name": "Quick Tools",
+            "blocks": [
+                {
+                    "opcode": "noteBlock",
+                    "blockType": "command",
+                    "text": "Note [NOTE]",
+                    "arguments": {
+                        "NOTE": {
+                            "type": "string",
+                            "defaultValue": "Enter your note"
+                        }
+                    }
+                }
+            ]
+        };
+    }
 
-    // Function to execute the code blocks inside the "do times" block
-    ext.doTimes = function (input, callback) {
-        for (var i = 0; i < input; i++) {
-            callback();
-        }
-    };
+    noteBlock({ NOTE }) {
+        // Here, you can perform any actions you want with the note, such as storing it in a variable or displaying it.
+        console.log("Note:", NOTE);
+    }
+}
 
-    // Function to execute the code blocks inside the "then do times" block
-    ext.thenDoTimes = function (input, callback) {
-        for (var i = 0; i < input; i++) {
-            callback();
-        }
-    };
-
-    // Extension initialization
-    ext._shutdown = function () {};
-
-    ext._getStatus = function () {
-        return { status: 2, msg: 'Ready' };
-    };
-
-    // Register the extension
-		ScratchExtensions.register('Quick Tools', descriptor, ext);
-})(window.ScratchExtensions || (window.ScratchExtensions = {}));
+Scratch.extensions.register(new ScratchFetch());
