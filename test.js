8b3a78259decc4af63b16fa966a7dde38603c114
ChucklesTheGuineaPig <112353565+SiriusTheKittenLab@users.noreply.github.com>
2023-05-24 19:55:04 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,18 +3,30 @@ class NitroBlock {
         return {
             "id": "NitroBlock",
             "name": "NitroBlock",
-            "blocks": [
-                {
-                    "opcode": "test",
-                    "blockType": "command",
-                    "text": "test"
-                }
-            ],
-            "menus": {}
-        };
-    }
-  
-    test() {
-        // Do nothing
-    }
+            "blocks": [{
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    substringy({num1, num2, string}) {
+        return string.substring(num1 - 1, num2);
+    };
 }
